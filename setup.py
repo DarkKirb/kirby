4b53cb6c33cff96367e91b11892d020fc08830c1
Morten Delenk <morten@dark32.cf>
2018-03-19 20:41:51 +0100

    remove: all old files

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from setuptools import setup, find_packages
 setup(
         name="Kirbytools",
         version="0.1",
-        packages=["kirby","kirby.mint"],
+        packages=["kirby","kirby.utils"],
         author="Dark Kirb",
         description="Tools for editing games using the Return to Dream Land engine",
         license="BSD-2clause",
-        entry_points={
-            'console_scripts':["read_yaml=kirby.xyaml:c_read_yaml",
-                               "write_yaml=kirby.xyaml:c_write_yaml"]
-        },
+        entry_points={},
         install_requires=["PyYAML>=3.12"]
         )
 
